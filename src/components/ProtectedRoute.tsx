4d89d8161c7e1f67bcc2da4a46756ea9d7dde5f3
CHЯISΓФPHΞЯ HΞDЯICH <Christopherhedrich@gmail.com>
2025-11-05 07:47:25 -0400

    auth

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuthHook';
+import { useAuth } from '../hooks/useAuth';
 import { Shield } from 'lucide-react';
 import { type Permission } from '../config/roles';
 
