758008289d90ac3c836e36b2c39da9dae91b6638
CHЯISΓФPHΞЯ HΞDЯICH <Christopherhedrich@gmail.com>
2025-11-06 15:22:23 -0400

    z

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,11 +6,5 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-
   },
 });
